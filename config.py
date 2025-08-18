6099eb5e1a66aa38e559e823490dd93fa6e199c5
cz040209 <leecz-wm23@student.tarc.edu.my>
2025-08-18 13:33:21 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
-customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
-customregion = "us-east-1"
+customdb = "employee1"
+custombucket = "leechenzhun-employee1"
+customregion = "us-east-1a"
 
