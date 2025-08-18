70cc0da07655cbe84a96f134cc97640a5a789d10
cz040209 <leecz-wm23@student.tarc.edu.my>
2025-08-18 13:36:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee1.cfj4syu9s2iw.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "Abcd+1314"
 customdb = "employee1"
 custombucket = "leechenzhun-employee1"
 customregion = "us-east-1a"
