b119a2c0a5e866d478ad4a0f2221ec01054226ac
cz040209 <leecz-wm23@student.tarc.edu.my>
2025-08-18 13:35:19 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee1.cfj4syu9s2iw.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee1"
